9466bab6351147e9cecafa798280bc7bee3df952
Olof Hagsand <olof@hagsand.se>
2016-02-28 17:23:55 +0100

    docker

--- example/docker/run.sh
+++ example/docker/run.sh
@@ -2,7 +2,7 @@
 # Start daemon and a cli docker containers .
 # Note that they have a common file-system at /data 
 #
-sudo docker run -td --net host -v $(pwd)/data:/data olofhagsand/clicon_backend
-sudo docker run -ti --rm --net host -v $(pwd)/data:/data olofhagsand/clicon_cli
+sudo docker run -td --net host -v $(pwd)/data:/data olofhagsand/clixon_backend
+sudo docker run -ti --rm --net host -v $(pwd)/data:/data olofhagsand/clixon_cli
 
 
