2a2785090bc33fbf904e552c348ed01934cd1deb
Olof hagsand <olof@hagsand.se>
2019-02-04 14:31:46 +0100

    travis libfcgi-dev

--- test/travis/before_script.sh
+++ test/travis/before_script.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+# Run this as before_script in the travis file
+apt-get update && apt-get install -y \
+  libfcgi-dev 
+
 git clone https://github.com/olofhagsand/cligen.git
 (cd cligen && ./configure && make && sudo make install)
