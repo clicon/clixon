84d2efdedca445407b95ad764f82bdfa394b199f
Olof Hagsand <olof@hagsand.se>
2019-02-11 14:37:07 +0100

    travis ./start.sh patch

--- test/travis/before_script.sh
+++ test/travis/before_script.sh
@@ -11,6 +11,6 @@ sudo groupadd clicon
 sudo usermod -a -G clicon $(whoami)
 sudo usermod -a -G clicon www-data
 # Build and start the system docker container
-(cd docker/system && make docker && start.sh)
+(cd docker/system && make docker && ./start.sh)
 # Run clixon testcases
 (cd docker/system && sudo docker exec -it clixon-system bash -c 'cd /clixon/clixon/test; exec ./all.sh')
