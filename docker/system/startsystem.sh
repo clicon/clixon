598ab84c157084a455c9c3a2dd25313d9db6c1ca
Olof hagsand <olof@hagsand.se>
2019-02-26 17:09:05 +0100

    skip test_startup

--- docker/system/startsystem.sh
+++ docker/system/startsystem.sh
@@ -49,7 +49,7 @@ EOF
 # This is a clixon site test file. Disable all model testing.
 cat <<EOF > /usr/local/bin/test/site.sh
 # Add your local site specific env variables (or tests) here.
-SKIPLIST="test_yangmodels.sh test_openconfig.sh test_install.sh test_order.sh"
+SKIPLIST="test_yangmodels.sh test_openconfig.sh test_install.sh test_order.sh test_startup.sh"
 #IETFRFC=
 EOF
 
