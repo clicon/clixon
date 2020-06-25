c11f3ba8ca07556c996904ea3d4995ee855c813b
shmuelnatan <67059909+shmuelnatan@users.noreply.github.com>
2020-06-25 11:32:16 +0300

    Update clixon_netconf_lib.c
    changed _namespace to namespace_

--- lib/src/clixon_netconf_lib.c
+++ lib/src/clixon_netconf_lib.c
@@ -568,14 +568,14 @@ netconf_unknown_element_xml(cxobj **xret,
 int
 netconf_unknown_namespace(cbuf *cb,
 			  char *type,
-			  char *_namespace,
+			  char *namespace_,
 			  char *message)
 {
     int    retval = -1;
     cxobj *xret = NULL;
 
     if (netconf_common_xml(&xret, type, "unknown-namespace",
-			   "bad-namespace", _namespace, message) < 0)
+			   "bad-namespace", namespace_, message) < 0)
 	goto done;
     if (clicon_xml2cbuf(cb, xret, 0, 0, -1) < 0)
 	goto done;
@@ -589,11 +589,11 @@ netconf_unknown_namespace(cbuf *cb,
 int
 netconf_unknown_namespace_xml(cxobj **xret,
 			      char   *type,
-			      char   *_namespace,
+			      char   *namespace_,
 			      char   *message)
 {
     return netconf_common_xml(xret, type, "unknown-namespace",
-			      "bad-namespace", _namespace, message);
+			      "bad-namespace", namespace_, message);
 }
 
 /*! Create Netconf access-denied error cbuf according to RFC 6241 App A
