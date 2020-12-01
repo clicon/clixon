1cd6d37fa14f7aba8a0399cb575666bde7c60b61
Olof hagsand <olof@hagsand.se>
2020-12-01 14:07:46 +0100

    test copy yang from /usr/local/share

--- test/test_restconf.sh
+++ test/test_restconf.sh
@@ -23,8 +23,8 @@ APPNAME=example
 cfg=$dir/conf.xml
 
 # clixon-example and clixon-restconf is used, copy here
-cp ../example/main/clixon-example@2020-03-11.yang $dir/
-cp ../yang/clixon/clixon-restconf@2020-10-30.yang $dir/
+cp /usr/local/share/clixon/clixon-example@2020-03-11.yang $dir/
+cp /usr/local/share/clixon/clixon-restconf@2020-10-30.yang $dir/
 
 cat <<EOF > $cfg
 <clixon-config xmlns="http://clicon.org/config">
