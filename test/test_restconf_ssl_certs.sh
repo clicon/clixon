df05acdc9e464af132dd7a7cf4277f12c2733e2c
Olof Hagsand <olof@hagsand.se>
2021-06-26 21:39:57 +0000

    test arm curl different return values

--- test/test_restconf_ssl_certs.sh
+++ test/test_restconf_ssl_certs.sh
@@ -291,7 +291,7 @@ EOF
     expectpart "$(curl $CURLOPTS --key $certdir/limited.key --cert $certdir/limited.crt -X GET $RCPROTO://localhost/restconf/data/example:x 2>&1)" "16 35 55 56" # 55 "certificate expired"
 
     new "too weak cert (sign w md5)"
-    expectpart "$(curl $CURLOPTS --key $certdir/mymd5.key --cert $certdir/mymd5.crt -X GET $RCPROTO://localhost/restconf/data/example:x 2>&1)" 58 "md too weak"
+    expectpart "$(curl $CURLOPTS --key $certdir/mymd5.key --cert $certdir/mymd5.crt -X GET $RCPROTO://localhost/restconf/data/example:x 2>&1)" "35 58" # "md too weak"
 
 # Havent been able to generate "wrong CA"
 #    new "invalid cert from wrong CA"
