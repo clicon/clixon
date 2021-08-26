7794c619ccfa34c3ad41ac07bd5bc9d6496d8dfd
Olof hagsand <olof@hagsand.se>
2021-08-26 18:45:12 +0200

    test: curl error codes in github/actions

--- test/test_restconf_http_upgrade.sh
+++ test/test_restconf_http_upgrade.sh
@@ -99,7 +99,7 @@ function testrun()
 	# http/2-only is always an error in http/1 + http/2 mode
 	new "restconf http2 prior-knowledge (h2:$h2enable)"
 	echo "curl -Ssik --http2-prior-knowledge -X GET http://localhost/.well-known/host-meta"
-	expectpart "$(curl -Ssik --http2-prior-knowledge -X GET http://localhost/.well-known/host-meta 2>&1)" "16 55"
+	expectpart "$(curl -Ssik --http2-prior-knowledge -X GET http://localhost/.well-known/host-meta 2>&1)" "16 52 55"
 
     elif [ ${HAVE_LIBNGHTTP2} = true -a ${HAVE_LIBEVHTP} = false ]; then  # http/2 only
 
@@ -160,7 +160,7 @@ function testrun()
 	# http/2-only is always an error in http/1 + http/2 mode
 	new "restconf http2 prior-knowledge (h2:$h2enable)"
 	echo "curl -Ssik --http2-prior-knowledge -X GET http://localhost/.well-known/host-meta"
-	expectpart "$(curl -Ssik --http2-prior-knowledge -X GET http://localhost/.well-known/host-meta 2>&1)" "16 55"
+	expectpart "$(curl -Ssik --http2-prior-knowledge -X GET http://localhost/.well-known/host-meta 2>&1)" "16 52 55"
 
     fi
 
