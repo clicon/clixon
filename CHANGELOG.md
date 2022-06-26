cd5b68b0b8d74f625b871b7a003b7616dd23b1a8
Olof hagsand <olof@hagsand.se>
2022-06-26 11:39:58 +0200

    Adapt to clixon master

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -47,7 +47,7 @@
   * New configure options:
     * `--enable-netsnmp`
     * `--with-mib-generated-yang-dir=DIR`
-  * Thanks Siklu for sponshoring 
+  * Thanks to Siklu Communications LTD for sponshoring this work
 
 ## 5.8.0
 Planned: July 2022
--- apps/snmp/snmp_handler.c
+++ apps/snmp/snmp_handler.c
@@ -372,7 +372,7 @@ snmp_scalar_set(clicon_handle               h,
 	clicon_err(OE_UNIX, errno, "cbuf_new");
 	goto done;
     }
-    if (clicon_xml2cbuf(cb, xtop, 0, 0, -1) < 0)
+    if (clixon_xml2cbuf(cb, xtop, 0, 0, -1, 0) < 0)
 	goto done;
     if (clicon_rpc_edit_config(h, "candidate", OP_MERGE, cbuf_get(cb)) < 0)
 	goto done;
