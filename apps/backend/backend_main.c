3d64eddaf2ced1287e06d2d2385ad24198c35c53
Olof hagsand <olof@hagsand.se>
2023-02-02 14:46:53 +0100

    All calls to ca_reset plugin callback has db=tmp

--- apps/backend/backend_main.c
+++ apps/backend/backend_main.c
@@ -905,8 +905,13 @@ main(int    argc,
             goto done;
     case SM_NONE: /* Fall through *
                    * Load plugins and call plugin_init() */
-        if (clixon_plugin_reset_all(h, "running") < 0)   
+        if (xmldb_db_reset(h, "tmp") < 0)
             goto done;
+        if (clixon_plugin_reset_all(h, "tmp") < 0)
+            goto done;
+        /* Copy tmp to running after reset where extra xml may have been introduced */
+        if (xmldb_copy(h, "tmp", "running") < 0)
+            goto done;        
         status = STARTUP_OK;
         break;
     case SM_RUNNING: /* Use running as startup */
