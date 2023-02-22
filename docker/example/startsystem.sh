a8c4e8699cac7235184edbafb798110027a73972
Olof hagsand <olof@hagsand.se>
2023-02-22 14:08:38 +0100

    Docker: Fixed broken config file in clixon-example

--- docker/example/startsystem.sh
+++ docker/example/startsystem.sh
@@ -54,6 +54,51 @@ touch /run/openrc/softlevel
 #/usr/local/bin/clixon_restconf -l f/var/log/restconf.log -D $DBG &
 #>&2 echo "clixon_restconf started"
 
+cat <<EOF > /usr/local/etc/example.xml
+<clixon-config xmlns="http://clicon.org/config">
+  <CLICON_CONFIGFILE>/usr/local/etc/example.xml</CLICON_CONFIGFILE>
+  <CLICON_FEATURE>ietf-netconf:startup</CLICON_FEATURE>
+  <CLICON_FEATURE>ietf-netconf:confirmed-commit</CLICON_FEATURE>
+  <CLICON_FEATURE>clixon-restconf:allow-auth-none</CLICON_FEATURE>
+  <CLICON_FEATURE>clixon-restconf:fcgi</CLICON_FEATURE>
+  <CLICON_YANG_DIR>/usr/local/share/clixon</CLICON_YANG_DIR>
+  <CLICON_YANG_DIR>@YANG_STANDARD_DIR@</CLICON_YANG_DIR>
+  <CLICON_YANG_MODULE_MAIN>clixon-example</CLICON_YANG_MODULE_MAIN>
+  <CLICON_CLI_MODE>example</CLICON_CLI_MODE>
+  <CLICON_BACKEND_DIR>/usr/local/lib/example/backend</CLICON_BACKEND_DIR>
+  <CLICON_NETCONF_DIR>/usr/local/lib/example/netconf</CLICON_NETCONF_DIR>
+  <CLICON_RESTCONF_DIR>/usr/local/lib/example/restconf</CLICON_RESTCONF_DIR>
+  <CLICON_CLI_DIR>/usr/local/lib/example/cli</CLICON_CLI_DIR>
+  <CLICON_CLISPEC_DIR>/usr/local/lib/example/clispec</CLICON_CLISPEC_DIR>
+  <CLICON_SOCK>/usr/local/var/example/example.sock</CLICON_SOCK>
+  <CLICON_BACKEND_PIDFILE>/usr/local/var/example/example.pidfile</CLICON_BACKEND_PIDFILE>
+  <CLICON_CLI_LINESCROLLING>0</CLICON_CLI_LINESCROLLING>
+  <CLICON_CLI_TAB_MODE>0</CLICON_CLI_TAB_MODE>
+  <CLICON_XMLDB_DIR>/usr/local/var/example</CLICON_XMLDB_DIR>
+  <CLICON_STARTUP_MODE>init</CLICON_STARTUP_MODE>
+  <CLICON_NACM_MODE>disabled</CLICON_NACM_MODE>
+  <CLICON_STREAM_DISCOVERY_RFC5277>true</CLICON_STREAM_DISCOVERY_RFC5277>
+  <CLICON_YANG_LIBRARY>false</CLICON_YANG_LIBRARY>
+  <restconf>
+    <enable>true</enable>
+    <auth-type>none</auth-type>
+    <socket><namespace>default</namespace><address>0.0.0.0</address><port>80</port><ssl>false</ssl></socket>
+  </restconf>
+  <autocli>
+     <module-default>false</module-default>
+     <list-keyword-default>kw-nokey</list-keyword-default>
+     <treeref-state-default>false</treeref-state-default>
+     <edit-mode-default>list container</edit-mode-default>
+     <completion-default>true</completion-default>
+     <rule>
+       <name>include clixon-example</name>
+       <module-name>clixon-example</module-name>
+       <operation>enable</operation>
+     </rule>
+  </autocli>
+</clixon-config>
+EOF
+
 ln -s /usr/local/etc/example.xml clixon.xml
 
 # Start clixon backend 
