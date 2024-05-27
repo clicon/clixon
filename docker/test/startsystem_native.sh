b8599558d4969760f61331eb0909846ff50dff5b
Olof hagsand <olof@hagsand.se>
2024-05-27 14:52:49 +0200

    Test: disable ssl client cert tests (temporarily)

--- docker/test/startsystem_native.sh
+++ docker/test/startsystem_native.sh
@@ -58,7 +58,7 @@ echo "$STORE" > /usr/local/var/example/running_db
 # - test_install.sh since you dont have the make environment
 cat <<EOF > /usr/local/bin/test/site.sh
 # Add your local site specific env variables (or tests) here.
-SKIPLIST="test_api.sh test_client.sh test_c++.sh test_install.sh test_privileges.sh"
+SKIPLIST="test_api.sh test_client.sh test_c++.sh test_install.sh test_privileges.sh test_restconf_ssl_certs.sh"
 EOF
 
 # Patch to override YANG_INSTALLDIRS
