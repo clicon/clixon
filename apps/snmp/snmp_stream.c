97a3b57e41a990e32342493595641fffcf80d0bb
Mico Micic <m.micic@mobatime.com>
2024-07-29 10:01:06 +0200

    Set pointer to NULL after free

--- apps/snmp/snmp_stream.c
+++ apps/snmp/snmp_stream.c
@@ -179,10 +179,14 @@ add_snmp_var_bindings(netsnmp_variable_list **var_list,
                     clixon_debug(CLIXON_DBG_SNMP, "type %s not supported in snmp trap", yang_key2str(yang_keyword_get(ychild)));
                     break;
             }
-            if (xmlstr)
+            if (xmlstr) {
                 free(xmlstr);
-            if (snmpval)
+                xmlstr = NULL;
+            }
+            if (snmpval) {
                 free(snmpval);
+                snmpval = NULL;
+            }
         } else {
             clixon_debug(CLIXON_DBG_SNMP, "no yang def found for %s", node_name);
         }
